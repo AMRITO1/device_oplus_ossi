8f247e852014160094f669285d6e8ea8dc75fcb7
Taki / <amrito@myyahoo.com>
2024-02-22 09:46:04 +0530

    ossi: Initial skeleton tree
    Signed-off-by: Amrito1 <bhabanidas431@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/oplus/ossi
+
+# Inherit the proprietary files
+include vendor/oplus/ossi/BoardConfigVendor.mk
+
